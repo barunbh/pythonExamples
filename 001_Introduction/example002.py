ba62b59650df5432a2d3eb626910c55332abca4b
Barun <bizs.barun@gmail.com>
2017-08-04 15:57:30 +0530

    second update

--- 001_Introduction/example002.py
+++ 001_Introduction/example002.py
@@ -2,9 +2,10 @@
 # End of lines are automatically included in the string, but it’s possible to prevent this by adding a \ at the end of the line.
 
 print("""
-Usage: thingy [OPTIONS]
-     -h                        Display this usage message
-     -H hostname               Hostname to connect to
+Welcome to Exenario:
+Benefits: Learn Explore Share Connect
+     -  Java           Display this usage message
+     -  Python         Hostname to connect to
 
-Welcome to Exenario
+Stay Tune, Stay Connected!!
 """)
--- test.py
+++ test.py
@@ -1,36 +1,15 @@
-import string
-import random
-from base64 import b64encode, b64decode
+# Definition of radius
+r = 2
 
-FLAG = 'flag{xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx}'
+# Import the math package
+import math
 
-enc_ciphers = ['rot13', 'b64e', 'caesar']
-# dec_ciphers = ['rot13', 'b64d', 'caesard']
+# Calculate C
+C = 2*math.pi*r
 
-def rot13(s):
-	_rot13 = string.maketrans(
-    	"ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz",
-    	"NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm")
-	return string.translate(s, _rot13)
+# Calculate A
+A = math.pi*(r**2)
 
-def b64e(s):
-	return b64encode(s)
-
-def caesar(plaintext, shift=3):
-    alphabet = string.ascii_lowercase
-    shifted_alphabet = alphabet[shift:] + alphabet[:shift]
-    table = string.maketrans(alphabet, shifted_alphabet)
-    return plaintext.translate(table)
-
-def encode(pt, cnt=50):
-	tmp = '2{}'.format(b64encode(pt))
-	for cnt in xrange(cnt):
-		c = random.choice(enc_ciphers)
-		i = enc_ciphers.index(c) + 1
-		_tmp = globals()[c](tmp)
-		tmp = '{}{}'.format(i, _tmp)
-
-	return tmp
-
-if __name__ == '__main__':
-	print (encode(FLAG, cnt=?))
+# Build printout
+print("Circumference: " + str(C))
+print("Area: " + str(A))
